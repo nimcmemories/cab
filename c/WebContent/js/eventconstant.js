206e71da0619c40fb8d3384c61ed5b802bbed053
nitinvavdiya <nitinvavdiya@gmail.com>
2014-10-14 22:25:28 +0530

    set limit for event constant

--- c/WebContent/js/eventconstant.js
+++ c/WebContent/js/eventconstant.js
@@ -1,5 +1,11 @@
 /**
  * 
  */
+//1-100 use for Taxi
+//101-200 use for Bus
+//201-300 use truck
+//301-400 use bike
 var eventId={};
 eventId.taxi_ptop=1;
+eventId.taxi_got_to_airport=2;
+eventId.taxi_pickup_from_airport=3;
